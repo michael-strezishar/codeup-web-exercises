113d60f4d1cc4e4371aa338eded2ff26486c2eaf
michaelstrezishar <michael.strezishar@gmail.com>
2018-08-08 14:44:04 -0500

    JS exercises

---
+++ JS_exercises.js
@@ -0,0 +1,24 @@
+!<DOCTYPE html>
+
+
+
+
+
+
+var googlePrice = 400;
+var amazonPrice = 380;
+var facebookPrice = 350;
+var googleHrs = 6;
+var amazonHrs = 4;
+var facebookHrs = 10;
+var googleTotal = googleHrs * googlePrice;
+var facebookTotal = facebookHrs * facebookPrice;
+var amazonTotal = amazonHrs * amazonPrice;
+
+console.log("Google paid" + googleTotal)("Facebook paid" + facebookTotal) ("Amazon paid" + amazonTotal);
+
+console.log("The total for all three companies is" + googleTotal + facebookTotal + amazonTotal);
+
+
+
+
