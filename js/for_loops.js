c644188ce44c71e94742b7a5593601539f4b0982
michaelstrezishar <michael.strezishar@gmail.com>
2018-08-13 17:03:01 -0500

    while loop exercise

---
+++ js/for_loops.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function showMultiplicationTable(number) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + (number * i));
+    }
+}
+showMultiplicationTable(8);
+//
+
+
+var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+for (var i = 0; i <= 10; i++){
+    if (i === 0){
+        console.log(i + " is even");
+    }
+    else if (i % 2 === 0){
+        console.log(i + " is even");
+
+    }
+    else {
+        console.log(i + " is odd");
+    }
+}
+//
+//
+for (var i = 100; i > 0; i-=5){
+    console.log(i);
+}
+
+var num = "";
+var value = 9;
+for (var i = 1; i <= value; i++) {
+    num = "";
+    for (var j = 1; j <= i; j++) {
+        num += i;
+
+    }
+    console.log(num);
+}
