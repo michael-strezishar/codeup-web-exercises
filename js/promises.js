2734aa3477d784083dd0323f49f4094bb5db90f9
michaelstrezishar <michael.strezishar@gmail.com>
2018-09-11 07:57:17 -0500

    lastpush exercise

---
+++ js/promises.js
@@ -0,0 +1,34 @@
+// function wait (n) {
+//     const numPromise = new Promise ((resolve, reject) => {
+//         setTimeout(() => resolve(), n);
+//     });
+//     return numPromise;
+//     };
+//
+// wait(1000).then(()=> console.log("You\ll see this after 1 second"));
+// wait(4000).then(()=> console.log("You\ll see this after 4 seconds"));
+
+// fetch(www.github.com, {headers: {"Authorization": " token d093c2881d261c18ad2f25e2fe96a6d3b00bd891"}})
+//
+// function gitHubName () {
+//     return fetch("https://api.github.com/michael-strezishar")
+//         .then(response => response.json())
+//
+//
+//
+// }
+//
+// console.log(gitHubName());
+
+const lastPush = username => {
+
+    return fetch('https://api.github.com/users/' + username + '/events', {headers: {'Authorization': 'token d093c2881d261c18ad2f25e2fe96a6d3b00bd891'}})
+        .then(response => response.json())
+        .then(e => e.filter(event => event.type === 'PushEvent'))
+        .then (event => event[0].created_at.substr(0, 10))
+        .catch(err => console.log(err));
+};
+
+    lastPush('michael-strezishar')
+    .then(data => console.log('Last Push:', data));
+
